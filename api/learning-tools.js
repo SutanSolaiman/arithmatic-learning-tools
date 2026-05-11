2c8fe10906cea0d8ee4d02443d2fcb4730185b6e
SutanSolaiman <sutan.solaiman@gmail.com>
2026-05-11 19:48:25 +0700

    Update learning-tools.js

--- api/learning-tools.js
+++ api/learning-tools.js
@@ -3,6 +3,14 @@ const APPS_SCRIPT_BACKEND_URL =
 
 export default async function handler(req, res) {
   try {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+      return res.status(204).end();
+    }
+
     if (req.method === "GET") {
       return res.status(200).json({
         ok: true,
